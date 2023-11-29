ea0317437c32e7fcca06ba60f2a4113db2e196a1
nonkrub123 <tichanon.k@gmail.com>
2023-11-29 11:38:04 +0700

    Add gui button

--- main.py
+++ main.py
@@ -11,8 +11,42 @@ class OurCalculator:
         self.label = tk.Label(self.root, text='Hello DIP01', font=('Arial', 18))
         self.label.pack()
 
-        self.button = tk.Button(self.root,text=('click here'))
-        self.button.place(x=0,y=0)
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('1'))
+        self.button.place(x=0,y=30)
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('2'))
+        self.button.place(x=60,y=30)
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('3'))
+        self.button.place(x=120,y=30)
+        
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('4'))
+        self.button.place(x=0,y=90)                 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('5'))
+        self.button.place(x=60,y=90)  
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('6'))
+        self.button.place(x=120,y=90) 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('7'))
+        self.button.place(x=0,y=150) 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('8'))
+        self.button.place(x=60,y=150) 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('9'))
+        self.button.place(x=120,y=150) 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('C'))
+        self.button.place(x=0,y=210) 
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('0'))
+        self.button.place(x=60,y=210)
+
+        self.button = tk.Button(self.root, font=('Arial', 18), height=1,width=3 ,   text=('='))
+        self.button.place(x=120,y=210)
 
         self.root.mainloop()
 OurCalculator()
